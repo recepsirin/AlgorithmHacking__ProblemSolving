2807cd9fdb6ae02fe3ba22c56346e53e90f29abc
Recep <recep4sirin@gmail.com>
2020-11-09 23:09:48 +0300

    algorithm built

--- perfect_number.py
+++ perfect_number.py
@@ -1,3 +1,19 @@
 def perfect_number(attained_list):
     """Method signature will be updated asap"""
-    pass
+    for n in attained_list:
+
+        if n != 1:
+            continue
+
+        total = 1
+
+        m = 2
+        while m * m <= n:
+            if n % m == 0:
+                total += m + n / m
+            m += 1
+
+        if total == n:
+            print("equal")
+        else:
+            print("not equal")
